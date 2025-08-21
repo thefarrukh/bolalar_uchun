f3abd6361343d1a0e2954a3c57728b9d59e40c90
thefarrukh <farruxfayzullayev01@gmail.com>
2025-08-21 14:23:29 +0500

    fix error

--- core/settings/base.py
+++ core/settings/base.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 
